4943040cdd03798570fd6d2c54e6a0debcdc88fb
davidsl <david.lange@geodata.no>
2020-07-18 22:02:07 +0200

    Updates

---
+++ precache-manifest.2e9a3f4bb9835b812ec07be1ed7608aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3a582d3b99f11a5a082e78d1a44269",
+    "url": "/cssanimation/index.html"
+  },
+  {
+    "revision": "fb30c13acb19d7fda30f",
+    "url": "/cssanimation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d33a6f71a3558f63e9d",
+    "url": "/cssanimation/static/js/2.b0395a54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cssanimation/static/js/2.b0395a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb30c13acb19d7fda30f",
+    "url": "/cssanimation/static/js/main.5ce134f2.chunk.js"
+  },
+  {
+    "revision": "048f6b42cceb2d5bb8ee",
+    "url": "/cssanimation/static/js/runtime-main.c691a170.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cssanimation/static/media/logo.5d5d9eef.svg"
+  }
+]);
